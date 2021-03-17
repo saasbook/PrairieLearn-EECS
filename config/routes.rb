3a6a6bee3e92439ca515bc1806eefd5c55c257b6
sanwu Luo <sanwuluo@sanwus-MacBook-Pro.local>
2021-03-18 06:41:19 +0800

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :questions
-  root to: 'questions#index', as: 'root'
+  root to: 'login#home', as: 'root'
 
   get '/home' => 'login#home', :as => :home
   get '/login' => 'login#login', :as => :login
