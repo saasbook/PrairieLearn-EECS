5105da406510bc750efba2edc5b3c12ade638e88
Faiz Shamji <fshamji@berkeley.edu>
2021-04-12 16:48:07 -0700

    added github login, removed dummy login, removed user profile view

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -10,7 +10,7 @@ class LoginController < ApplicationController
 
   def home
     if session[:current_user_id].present?
-      redirect_to root
+      redirect_to user_profile_path
     end
   end
 
@@ -19,18 +19,13 @@ class LoginController < ApplicationController
     redirect_to home_path, notice: 'You have successfully logged out.'
   end
 
-  def dummylogin
-    session[:current_user_id] = 123
-    redirect_to home_path
-  end
-
   private
 
   def create_session(create_if_not_exists)
     user_info = request.env['omniauth.auth']
     user = find_or_create_user(user_info, create_if_not_exists)
     session[:current_user_id] = user.id
-    destination_url = session[:destination_after_login] || root_url
+    destination_url = session[:destination_after_login] || repo_path
     session[:destination_after_login] = nil
     redirect_to destination_url
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,12 @@ Rails.application.routes.draw do
 
   get '/home' => 'login#home', :as => :home
   get '/login' => 'login#login', :as => :login
-  get '/dummylogin' => 'login#dummylogin', :as => :dummylogin
   get '/login/github', to: redirect('auth/github'), as: :github_login
   match '/auth/github/callback', to: 'login#github', as: :github_callback, via: [:get, :post]
   get '/logout' => 'login#logout', :as => :logout
   get '/user/profile', to: 'questions#index', as: :user_profile
-  get '/repo', to: 'repo#new'
+  get '/repo', to: 'repo#new', as: 'repo'
   get '/sync', to: 'repo#sync', as: 'sync'
 
-  post '/auth/github/callback', to: 'login#github'
+  #post '/auth/github/callback', to: 'login#github'
 end
