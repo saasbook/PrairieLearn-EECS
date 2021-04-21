56b7a4e89ca9a12225f075b12aafe7273f2fc164
Timotius Vincent <timotius.vincent99@gmail.com>
2021-04-21 02:10:29 -0700

    added comments to model and controllers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+	# Display all questions pulled from the synced repo
 	def index
 		if session[:current_user_id].present? and session[:selected_repo].present?
 			@questions = Question.where(user_id: session[:current_user_id], repo: session[:selected_repo])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,11 @@
 # model for user
 class User < ApplicationRecord
-  # Add more Authentication Providers here.
+  # Add more Authentication Providers here
+  # It is unlikely that this app need other providers
   enum provider: {github: 1}, _prefix: :provider
 
-  # Each (uid, provider) pair should be unique.
+  # Each (uid, provider) pair should be unique
   validates :uid, uniqueness: { scope: :provider }
-
-  """
-  def auth_provider
-    {
-      'github' => 'Github'
-    }[provider]
-  end
-  """
 
   def self.find_github_user(uid)
     User.find_by(
