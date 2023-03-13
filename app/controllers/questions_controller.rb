56db3a702f179ec49aa7d8305ae36f0a674fbb7c
kcsama <609847732amy@gmail.com>
2023-03-12 23:07:37 -0700

    search bar done but need drying

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,7 @@
 class QuestionsController < ApplicationController
 	# Display all questions pulled from the synced repo
 	def index
+		@selected_questions = []
 		if session[:current_user_id].present? and session[:selected_repo].present?
 			current_user = User.find(session[:current_user_id])
 			if current_user != nil and current_user.repo == session[:selected_repo]
@@ -14,6 +15,9 @@ class QuestionsController < ApplicationController
 	def search
 		@search_terms = params[:search]
 		@questions = Question.where("title LIKE ?", "%#{@search_terms}%")
-		render "/questions/index"
+		respond_to do |format|
+			format.html { render partial: 'search_questions' }
+		end
+		#render 'questions/index'
 	end
 end
