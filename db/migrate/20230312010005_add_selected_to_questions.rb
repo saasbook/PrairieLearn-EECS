c994c9b67931451f160e5ece717e36f55a7f2896
Guobin Liang <a544266477@berkeley.edu>
2023-03-21 22:15:33 +0000

    delete the add migrating

--- db/migrate/20230312010005_add_selected_to_questions.rb
+++
@@ -1,5 +0,0 @@
-class AddSelectedToQuestions < ActiveRecord::Migration[6.1]
-  def change
-    add_column :questions, :selected, :string
-  end
-end
--- db/migrate/20230312010406_change_description_type_in_selected.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDescriptionTypeInSelected < ActiveRecord::Migration[6.1]
-  def change
-    change_column :questions, :selected, :boolean
-  end
-end
