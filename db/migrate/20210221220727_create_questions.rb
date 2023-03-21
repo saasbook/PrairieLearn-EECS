bfaf9dd748860dcd86d70444ed5f06e37493263e
Guobin Liang <a544266477@berkeley.edu>
2023-03-21 23:31:57 +0000

    update database

--- db/migrate/20210221220727_create_questions.rb
+++ db/migrate/20210221220727_create_questions.rb
@@ -3,7 +3,7 @@ class CreateQuestions < ActiveRecord::Migration[6.1]
     create_table :questions do |t|
    	  t.string :title
    	  t.string :repo
-      t.integer :selected
+      t.boolean :selected
     end
   end
 end
