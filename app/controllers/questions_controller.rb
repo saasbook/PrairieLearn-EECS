6d639407f87ea5ce12f395cd58d6d3381c7d4718
Guobin Liang <a544266477@berkeley.edu>
2023-04-26 22:25:49 +0000

    fix the search

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,22 +1,29 @@
 class QuestionsController < ApplicationController
 	# Display all questions pulled from the synced repo
 	def index
-		@selected_questions = []
-		if session[:current_user_id].present? and session[:selected_repo].present?
-			current_user = User.find(session[:current_user_id])
-			if current_user != nil and current_user.repo == session[:selected_repo]
-				@questions = Question.where(repo: session[:selected_repo])
-			end
-		else
-			@questions = Question.where(repo: "0")
-		end
+		@questions = find_user_repo
 	end
 
 	def search
+		@questions = find_user_repo
 		@search_terms = params[:search]
-		@questions = Question.where("title LIKE :search_terms OR descriptivetitle LIKE :search_terms", search_terms: Question.sanitize_sql_like(params[:search]) + "%")
+		@questions = @questions.where("title LIKE :search_terms OR descriptivetitle LIKE :search_terms", search_terms: Question.sanitize_sql_like(params[:search]) + "%")
 		respond_to do |format|
 			format.html { render partial: 'search_questions' }
 		end
+	end
+
+	private
+
+	def find_user_repo
+		if session[:current_user_id].present? and session[:selected_repo].present?
+			current_user = User.find(session[:current_user_id])
+			if current_user != nil and current_user.repo == session[:selected_repo]
+				questions = Question.where(repo: session[:selected_repo])
+			end
+		else
+			questions = Question.where(repo: "0")
+		end
+		questions
 	end
 end
